4347dd5101a6a515bd5ab0f38ffb009228749070
Younesse Kaddar <younesse.kaddar@chch.ox.ac.uk>
2024-09-28 20:40:57 +0100

    minor

--- agents/guardrails.py
+++ agents/guardrails.py
@@ -136,7 +136,6 @@ class NewNonIidGuardrail(Guardrail):
     def __init__(self, agent, threshold, alpha):
         super().__init__(agent, threshold)
         self.alpha = alpha
-        self.alpha_fraction = 1 / len(agent.log_posterior)
 
     def m_alpha(self):
         posterior = t.exp(self.agent.log_posterior)
@@ -146,7 +145,7 @@ class NewNonIidGuardrail(Guardrail):
             m_alpha[max_indices.item()] = True
         else:
             m_alpha[max_indices[0]] = True
-        m_alpha |= (posterior >= self.alpha * self.alpha_fraction)
+        m_alpha |= (posterior >= self.alpha / len(self.agent.log_posterior))
         return m_alpha
 
     def harm_estimate(self, action):
